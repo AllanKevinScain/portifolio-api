d9876a272d0e8d69c9e0654707876da114b246ce
allankevinscain <meuemail44allan@gmail.com>
2026-04-14 07:44:21 -0300

    fix: build rodando parte 4

--- src/main.ts
+++ src/main.ts
@@ -2,24 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter, ResponseInterceptor } from './interceptors';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  const config = app.get(ConfigService);
+  const port = parseInt(config.get('DB_PORT') ?? '3000', 10);
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      transform: true,
-      whitelist: true,
-    }),
-  );
-
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   app.useGlobalInterceptors(new ResponseInterceptor());
   app.useGlobalFilters(new HttpExceptionFilter());
 
-  const port = process.env.PORT || 3000;
-
-  await app.listen(port);
-  console.log(`✅ API rodando em http://localhost:${port}`);
+  await app.listen(config.get('DB_PORT') ?? 3000);
+  console.log(`✅ portifolio-api rodando em http://localhost:${port}`);
 }
 
 void bootstrap();
