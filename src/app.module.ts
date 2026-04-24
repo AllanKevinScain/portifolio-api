1790d4a0bceb6827426f7d95be46ca90900c5ffa
allankevinscain <meuemail44allan@gmail.com>
2026-04-24 16:28:41 -0300

    fix: migrations not running solved 12

--- src/app.module.ts
+++ src/app.module.ts
@@ -29,8 +29,8 @@ import databaseConfig, { DatabaseConfig } from './config/database.config';
 
         return {
           ...db,
-          entities: [`${__dirname}/**/*.entity{.js,.ts}`],
-          migrations: [`${__dirname}/migration/{*.ts,*.js}`],
+          entities: [`${__dirname}/**/*.entity.js`],
+          migrations: [`${__dirname}/migration/*.js`],
           migrationsRun: true,
           // synchronize: true,
         };
